23148e5c0fd59c0d5a15cbdcd7200bb383f7ff6f
Cristian Contreras <cristian.d.contreras@gmail.com>
2021-12-23 20:50:40 -0300

    Agregar archivos de proyecto.

---
+++ Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Login
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIniciarSesion_Click(object sender, EventArgs e)
+        {
+            
+            if((txtUsuario.Text=="Usuario1")&&(txtContraseña.Text=="1234"))
+            {
+              MessageBox.Show("Accediendo al sistema", "Bienvenido",MessageBoxButtons.OK,MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Usuario o Contraseña incorrectos", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtUsuario.Clear();
+                txtContraseña.Clear();
+                txtUsuario.Focus();
+            }
+            
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void txtUsuario_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar >= 32 && e.KeyChar <= 47) || (e.KeyChar >= 58 && e.KeyChar <= 64) || (e.KeyChar >= 91 && e.KeyChar <= 96) || (e.KeyChar >= 123 && e.KeyChar <= 255))
+            {
+                MessageBox.Show("Solo se permiten letras y numeros");
+                e.Handled = true;
+                return;
+            }
+        }
+        
+      
+    }
+}
